f0eaf42dacf37b7b0d6ea76a29308862d4094fee
Abel Muriithi <abelmuriithi04@gmail.com>
2023-03-27 01:57:16 +0300

    done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,14 @@
 class SessionsController < ApplicationController
     def create
         user = User.find_by(username: params[:username])
+
         if user&.authenticate(params[:password])
           session[:user_id] = user.id
           render json: user, status: :created
         else
           render json: {errors: ["Not authorized"]}, status: :unauthorized
         end
+        
       end
     
       def destroy
